d042f8077bb3168f2cf81054564f6bd0f618df14
radegast0 <fatih.yonucuoglu@gmail.com>
2024-03-21 04:29:13 +0300

    stars, camera adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import Navbar from './components/Navbar';
 import { Canvas } from '@react-three/fiber';
-import { Html, OrbitControls } from '@react-three/drei';
+import {
+	OrbitControls,
+	Environment,
+	PerspectiveCamera,
+} from '@react-three/drei';
 import Background from './components/Background';
-
-
 
 
 const App = () => {
 	return (
 		<>
-			<Canvas style={{ height: '100vh', width: '100wh' }}
-			camera={{ fov: 75, position: [0, 0, 6] }}
+			<Canvas
+				style={{ height: '100vh', width: '100wh', background: 'black' }}
+				camera={{ fov: 75 }}
 			>
-				<OrbitControls />
+				<PerspectiveCamera
+					makeDefault
+					position={[0, 0, 10]}
+				/>
+				{/* OrbitControls */}
+				<OrbitControls
+					enablePan={false}
+					maxDistance={30}
+					minPolarAngle={Math.PI / 4}
+					maxPolarAngle={Math.PI / 2}
+					minAzimuthAngle={-Math.PI / 3}
+					maxAzimuthAngle={Math.PI / 3}
+				/>
+
+				{/* Environment */}
+				<Environment preset="sunset" />
+
+				{/* Background */}
 				<Background />
-				<Html fullscreen>
-					<Navbar />
-				</Html>
 			</Canvas>
 		</>
 	);
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,23 +1,69 @@
-import { Plane } from "@react-three/drei"
+import { Plane, Stage, Stars } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+import { useRef } from 'react';
 
 const Background = () => {
-  return (
-    <>
-    <Plane args={[10,10]} position={[0,0,-6]}>
-      <meshBasicMaterial color={'hotpink'} />
-    </Plane>
-    <Plane args={[10,10]} position={[5,0,-1]} rotation={[0, -Math.PI / 2, 0]}>
-      <meshBasicMaterial color={'red'} />
-    </Plane>
-    <Plane args={[10,10]} position={[-5,0,-1]} rotation={[0, Math.PI / 2, 0]}>
-      <meshBasicMaterial color={'green'} />
-    </Plane>
-    <Plane args={[10,10]} position={[0,5,-1]} rotation={[Math.PI / 2, 0, 0]}>
-      <meshBasicMaterial color={'blue'} />
-    </Plane>
-    
-    </>
-  )
-}
+  const { size, mouse } = useThree();
+  const meshRef = useRef();
+  
+  useFrame(() => {
+    // Calculate rotation based on mouse position
+    const mouseX = mouse.x * size.width / 32;
+    const mouseY = mouse.y * size.height / 64;
+    const targetRotationX = (mouseX / size.width) * Math.PI;
+    const targetRotationY = -(mouseY / size.height) * Math.PI;
 
-export default Background
+    // Rotate the mesh
+    meshRef.current.rotation.x += (targetRotationY - meshRef.current.rotation.x) * 0.01;
+    meshRef.current.rotation.y += (targetRotationX - meshRef.current.rotation.y) * 0.05;
+  });
+
+
+	return (
+    <>
+    
+    
+    <Stars saturation={1} fade speed={1.5} />
+		<Stage adjustCamera>
+      <mesh ref={meshRef}>
+        <Plane
+          args={[10, 10]}
+          position={[0, 0, -6]}
+        >
+          <meshBasicMaterial color={'hotpink'} />
+        </Plane>
+        <Plane
+          args={[10, 10]}
+          position={[5, 0, -1]}
+          rotation={[0, -Math.PI / 2, 0]}
+        >
+          <meshBasicMaterial color={'red'} />
+        </Plane>
+        <Plane
+          args={[10, 10]}
+          position={[-5, 0, -1]}
+          rotation={[0, Math.PI / 2, 0]}
+        >
+          <meshBasicMaterial color={'green'} />
+        </Plane>
+        <Plane
+          args={[10, 10]}
+          position={[0, 5, -1]}
+          rotation={[Math.PI / 2, 0, 0]}
+        >
+          <meshBasicMaterial color={'blue'} />
+        </Plane>
+        <Plane
+          args={[10, 10]}
+          position={[0, -5, -1]}
+          rotation={[-Math.PI / 2, 0, 0]}
+        >
+          <meshBasicMaterial color={'yellow'} />
+        </Plane>
+      </mesh>
+    </Stage>
+    </>
+	);
+};
+
+export default Background;
