eddd73f5a0e46e8f6be611b172d1a8c5a5bb6e61
radegast0 <fatih.yonucuoglu@gmail.com>
2024-03-28 21:41:21 +0300

    nav responsive

--- src/html/Navbar.jsx
+++ src/html/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 	return (
 		<header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
 			<nav className="flex w-full screen-max-width ">
-				<div className="flex flex-1 justify-center gap-12 ">
+				<div className="flex flex-1 justify-center gap-0 lg:gap-12 text-sm lg:text-base ">
 					{navLists.map((nav) => (
 						<div
 							className=" px-4 p-1 cursor-pointer hover:text-white transition-all"
