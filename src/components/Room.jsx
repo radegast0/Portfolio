c51c91056791f20deb80b74ccf26a34cc05d8f18
radegast0 <fatih.yonucuoglu@gmail.com>
2024-06-03 00:36:31 +0300

    styling

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,47 +1,41 @@
-import { useGLTF, useTexture } from '@react-three/drei';
-import TvScreen from './TvScreen';
-
-
+import { useGLTF, useTexture } from "@react-three/drei";
+import TvScreen from "./TvScreen";
 
 const Room = () => {
+  const { nodes } = useGLTF("./models/room.glb");
+  const bakedTexture = useTexture("/assets/room-baked-v4.jpg");
 
-	const { nodes } = useGLTF('./models/room.glb');
-	const bakedTexture = useTexture('/assets/room-baked-v4.jpg');
+  return (
+    <>
+      <mesh
+        position={nodes.mergedV5.position}
+        geometry={nodes.mergedV5.geometry}
+        castShadow
+      >
+        <meshBasicMaterial map={bakedTexture} map-flipY={false} />
+      </mesh>
+      <mesh
+        position={nodes.frameLight.position}
+        geometry={nodes.frameLight.geometry}
+        scale={nodes.frameLight.scale}
+        rotation={nodes.frameLight.rotation}
+      />
+      <mesh
+        position={nodes.lampBulb.position}
+        geometry={nodes.lampBulb.geometry}
+        scale={nodes.lampBulb.scale}
+        rotation={nodes.lampBulb.rotation}
+      />
 
-	return (
-		<>
-			<mesh
-				position={nodes.mergedV5.position}
-				geometry={nodes.mergedV5.geometry}
-				castShadow
-			>
-				<meshBasicMaterial
-					map={bakedTexture}
-					map-flipY={false}
-				/>
-			</mesh>
-			<mesh
-				position={nodes.frameLight.position}
-				geometry={nodes.frameLight.geometry}
-				scale={nodes.frameLight.scale}
-				rotation={nodes.frameLight.rotation}
-			/>
-			<mesh
-				position={nodes.lampBulb.position}
-				geometry={nodes.lampBulb.geometry}
-				scale={nodes.lampBulb.scale}
-				rotation={nodes.lampBulb.rotation}
-			/>
-			
-			<mesh
-				position={nodes.shelfLight.position}
-				geometry={nodes.shelfLight.geometry}
-				scale={nodes.shelfLight.scale}
-				rotation={nodes.shelfLight.rotation}
-			/>
-			<TvScreen />
-		</>
-	);
+      <mesh
+        position={nodes.shelfLight.position}
+        geometry={nodes.shelfLight.geometry}
+        scale={nodes.shelfLight.scale}
+        rotation={nodes.shelfLight.rotation}
+      />
+      <TvScreen />
+    </>
+  );
 };
 
 export default Room;
